407c299eae2dc7435508f96e9d1d3f001bf7a733
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-05-17 10:57:43 +0100

    add greet component to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/greet/Greet'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Greet name='Cameron' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
