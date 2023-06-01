9c87a64f12a41aaa33f8daba40cbc591489fd313
Cameron Poole <110310005+cedekpoole@users.noreply.github.com>
2023-06-01 17:55:11 +0100

    practice with getByRole options

--- src/application/application.test.tsx
+++ src/application/application.test.tsx
@@ -4,10 +4,26 @@ import { Application } from "./application";
 describe('Application component', () => {
     test('component renders correctly', () => {
         render(<Application />);
+
+        const pageTitleEl = screen.getByRole('heading', {
+            level: 1
+        })
+        expect(pageTitleEl).toBeInTheDocument();
+
+        const sectionOneTitle = screen.getByRole('heading', {
+            level: 2
+        })
+        expect(sectionOneTitle).toBeInTheDocument();
+
         const nameEl = screen.getByRole('textbox', {
             name: "Name"
         });
         expect(nameEl).toBeInTheDocument();
+
+        const bioEl = screen.getByRole('textbox', {
+            name: "Bio"
+        });
+        expect(bioEl).toBeInTheDocument();
         
         const jobLocationEl = screen.getByRole("combobox");
         expect(jobLocationEl).toBeInTheDocument();
--- src/application/application.tsx
+++ src/application/application.tsx
@@ -1,32 +1,35 @@
-
-
 export const Application = () => {
-    return (
-        <form>
-            <div>
-                <label htmlFor="name">Name</label>
-                <input type="text" id="name" />
-            </div>
-            <div>
-                <label htmlFor="bio">Bio</label>
-                <textarea id="bio" name="bio"></textarea>
-            </div>
-            <div>
-                <label htmlFor="job-location">Job Location</label>
-                <select id="job-location">
-                    <option value="">Select a country</option>
-                    <option value="germany">Germany</option>
-                    <option value="spain">Spain</option>
-                    <option value="uk">United Kingdom</option>
-                    <option value="usa">United States of America</option>
-                </select>
-            </div>
-            <div>
-                <label>
-                    <input type="checkbox" id="terms"/> I agree with the terms and conditions
-                </label>
-            </div>
-            <button>Submit</button>
-        </form>
-    )
-}
+  return (
+    <>
+    <h1>Job Application Form</h1>
+    <h2>Section 1</h2>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" />
+        </div>
+        <div>
+          <label htmlFor="bio">Bio</label>
+          <textarea id="bio" name="bio"></textarea>
+        </div>
+        <div>
+          <label htmlFor="job-location">Job Location</label>
+          <select id="job-location">
+            <option value="">Select a country</option>
+            <option value="germany">Germany</option>
+            <option value="spain">Spain</option>
+            <option value="uk">United Kingdom</option>
+            <option value="usa">United States of America</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" id="terms" /> I agree with the terms and
+            conditions
+          </label>
+        </div>
+        <button>Submit</button>
+      </form>
+    </>
+  );
+};
